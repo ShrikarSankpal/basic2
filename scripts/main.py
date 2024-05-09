3d613e9e29ad16fce37ff04b963ebd48abeb02bb
ShrikarSankpal <shrikarsankpal25@gmail.com>
2024-05-09 13:17:45 +0530

    added a python script and input output files

--- scripts/main.py
+++ scripts/main.py
@@ -1,1 +1,14 @@
-print(5)
+def sum_numbers(input_file, output_file):
+	with open(input_file,"r") as f:
+		numbers = [int(line.strip()) for line in f]
+
+	results = sum(numbers)
+
+	with open(output_file,"w") as f:
+		f.write(str(results))
+
+if(__name__=="__main__"):
+	input_file = "/home/user/projects/basic2/input/input_file.dat"
+	output_file = "/home/user/projects/basic2/output/output_file.dat"
+
+	sum_numbers(input_file, output_file)
